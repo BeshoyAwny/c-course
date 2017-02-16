b73f52ef24f6fc3a18ea5acf89744fff0d6f47b5
Beshoy-laptop\Beshoy <Beshoy.a.lamey@hotmail.com>
2017-02-17 01:46:44 +0200

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime my_age =Convert.ToDateTime(Console.ReadLine());
+            int year =Convert.ToInt32( DateTime.Now.Subtract(my_age).TotalDays) / 360;
+            Console.WriteLine("your age is "+year + "year");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
